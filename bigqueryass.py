84a3759619bd683ee31372f9a7fc5c3f5f64a54e
saheed <saheedjimoh338@gmail.com>
2023-05-12 21:59:52 +0100

    vibe commit

--- bigqueryass.py
+++ bigqueryass.py
@@ -1,3 +1,5 @@
+#UPLOAD A TABLE
+
 from google.cloud import bigquery
 
 import pandas_gbq
@@ -5,40 +7,36 @@ import pandas
 import os
 
 
-# credential_path = r'C:\Users\HP\Documents\Alt_school\venv\plated-entry-385012-df33e4b65a90.json'
-# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
+credential_path = r'C:\Users\HP\Documents\Alt_school\venv\plated-entry-385012-df33e4b65a90.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 
-# client = bigquery.Client()
-# tanble_id = 'plated-entry-385012.garden.garden_sensors'
+client = bigquery.Client()
+tanble_id = 'plated-entry-385012.garden.garden_sensors'
 
-# rows_to_insert =[
-#     {u'sensor_name': 'garden_001', u'temperatur': 88.2, u'humudity': 23.8},
-#     {u'sensor_name': 'garden_002', u'temperatur': 38.2, u'humudity': 53.8},
-#     {u'sensor_name': 'garden_002', u'temperatur': 84.2, u'humudity': 83.8},
-#     {u'sensor_name': 'garden_002', u'temperatur': 38.2, u'humudity': 59.8}
+rows_to_insert =[
+    {u'sensor_name': 'garden_001', u'temperatur': 88.2, u'humudity': 23.8},
+    {u'sensor_name': 'garden_002', u'temperatur': 38.2, u'humudity': 53.8},
+    {u'sensor_name': 'garden_002', u'temperatur': 84.2, u'humudity': 83.8},
+    {u'sensor_name': 'garden_002', u'temperatur': 38.2, u'humudity': 59.8}
 
-# ]
+]
 
-# error = client.insert_rows_json(tanble_id, rows_to_insert)
-# if error == []:
-#     print("new rows as been added")
-# else:
-#     print(f'encounting error while inserting: {error}')
+error = client.insert_rows_json(tanble_id, rows_to_insert)
+if error == []:
+    print("new rows as been added")
+else:
+    print(f'encounting error while inserting: {error}')
 
-# # # # plated-entry-385012-df33e4b65a90
 
-##query the uploaded data
+## TO QUERY DATA
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'alt-school-project-386517-fcb14f08a9df.json'
-client = bigquery.Client()
 
-sql_code = """
+sql = """
 SELECT *
  FROM `alt-school-project-386517.alt_school_data.titanic`
-LIMIT 10
+ LIMIT 10
 """
 
-query_work = client.query(sql_code)
-
-for row in query_work.result():
-    print(row)
+df = pandas_gbq.read_gbq(sql)
+print(df)
